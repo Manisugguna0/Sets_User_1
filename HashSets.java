7c0076d556ec4b7b02acc7441400e14c5bf01437
Manisugguna0 <subrahmanyamanikanta@gmail.com>
2024-08-22 17:39:33 +0530

    Mani

--- HashSets.java
+++ HashSets.java
@@ -1,3 +1,18 @@
+
+import java.util.HashSet;
+
 public class HashSets{
 // Prepare Hashset object here and add person class objects in them iterate it by using for each
+public static void main(String args[]){
+    HashSet<Person> p=new HashSet<>();
+    p.add(new Person(1,"Mani",22));
+
+    p.add(new Person(1,"sai",22));
+
+    p.add(new Person(2,"yogi",22));
+
+ for(Person i:p){
+    System.out.println(i);
+ }
+}
 }
--- LinkedHashSets.java
+++ LinkedHashSets.java
@@ -1,4 +1,18 @@
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
 public class LinkedHashSets{
 public static void main(String[]args){
   // Preapre LinkedHashSets Object and Load Person Objects in it and iterate it using Iterator and While Loop
+   LinkedHashSet<Person> a=new LinkedHashSet<>();
+ a.add(new Person(54,"Mani",21));
+ a.add(new Person(5,"yogi",23));
+ a.add(new Person(96,"sai",22));
+ Iterator<Person> i=a.iterator();
+ while(i.hasNext()){
+  System.out.println(i.next());
+ }
+
 }
+}
+
